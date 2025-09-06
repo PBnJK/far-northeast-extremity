eb284f8abee5949bcd0300812f1a77dfad59e8ff
PBnJK <pedrobuitrago2007yahoo@gmail.com>
2025-09-06 14:27:29 -0300

    Basic MVP

--- script.js
+++ script.js
@@ -6,11 +6,19 @@
 
 "use strict";
 
+const main = document.getElementById("main");
+
+let oldWindowInnerWidth = window.innerWidth;
+
 class Item {
-  constructor(id, url, alt, under, x, y, w, h, z = 0) {
+  constructor(id, url, alt, x, y, w, h, z = 0) {
     this.id = id;
 
+    this.offsetX = 0;
+    this.offsetY = 0;
+
     /* Flags */
+    this.flagGrabbable = true;
     this.flagFirstGrabbed = false;
     this.flagFirstDropped = false;
 
@@ -30,14 +38,15 @@ class Item {
       }
 
       this.grabbedCallback?.();
+      if (this.flagGrabbable) {
+        this.element = this.getElement();
 
-      this.element = this.getElement();
+        this.offsetX = e.clientX - this.element.getBoundingClientRect().left;
+        this.offsetY = e.clientY - this.element.getBoundingClientRect().top;
+        this.element.classList.add("dragging");
 
-      this.offsetX = e.clientX - this.element.getBoundingClientRect().left;
-      this.offsetY = e.clientY - this.element.getBoundingClientRect().top;
-      this.element.classList.add("dragging");
-
-      document.addEventListener("mousemove", this.dragElement);
+        document.addEventListener("mousemove", this.dragElement);
+      }
     };
 
     this.dragElement = (e) => {
@@ -62,7 +71,7 @@ class Item {
       this.element = null;
     };
 
-    this.create(id, url, alt, under, w, h, z);
+    this.create(id, url, alt, main, w, h, z);
     this.moveTo(x, y);
   }
 
@@ -85,6 +94,18 @@ class Item {
     div.addEventListener("mousedown", this.startDragging);
     div.addEventListener("mouseup", this.stopDragging);
 
+    window.addEventListener("resize", () => {
+      const e = this.element ?? this.getElement();
+      const rect = e.getBoundingClientRect();
+
+      const oldX = rect.left;
+      const w = window.innerWidth - oldWindowInnerWidth;
+      const newX = w / 2 + oldX;
+
+      oldWindowInnerWidth = window.innerWidth;
+      this.moveTo(newX, rect.top);
+    });
+
     under.appendChild(div);
   }
 
@@ -96,6 +117,10 @@ class Item {
 
     e.style.left = this.offsetX + "px";
     e.style.top = this.offsetY + "px";
+  }
+
+  setGrabbable(grabbable) {
+    this.flagGrabbable = grabbable;
   }
 
   setFirstGrabbedCallback(callback) {
@@ -119,13 +144,55 @@ class Item {
   }
 }
 
-const i = new Item(
-  "tomb_key",
-  "assets/img_tomb_key.png",
-  "An icy key",
-  document.getElementById("pivot"),
-  600,
-  400,
-  64,
-  64,
-);
+const items = [];
+
+function spawnItem(item) {
+  items.push(item);
+}
+
+function spawnNormalPaper() {
+  const normal_paper = new Item(
+    "normal_paper",
+    "assets/img_normal_paper.png",
+    'A taped sheet of paper. It reads: "normal boring place, no secrets!"',
+    992,
+    528,
+    320,
+    120,
+  );
+
+  normal_paper.setFirstGrabbedCallback(() => {
+    window.setTimeout(() => {
+      const stinger = new Audio("assets/snd_dramatic_stinger.mp3");
+      stinger.play();
+    }, 500);
+  });
+
+  spawnItem(normal_paper);
+}
+
+function spawnFrozenTombSign() {
+  const frozen_tomb_sign = new Item(
+    "frozen_tomb_sign",
+    "assets/img_frozen_tomb_sign.png",
+    'A sign. It reads: "Frozen Tomb," with a red arrow pointing left',
+    600,
+    480,
+    180,
+    180,
+  );
+
+  frozen_tomb_sign.setGrabbable(false);
+  frozen_tomb_sign.setFirstGrabbedCallback(() => {
+    window.location.href = "/scenes/30/";
+  });
+
+  spawnItem(frozen_tomb_sign);
+}
+
+function init() {
+  spawnNormalPaper();
+  spawnFrozenTombSign();
+}
+
+init();
