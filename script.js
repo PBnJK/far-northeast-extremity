f1ed6072c756fe364ce6ea30ea02b6ac08e52d29
PBnJK <pedrobuitrago2007yahoo@gmail.com>
2025-09-06 22:08:55 -0300

    reception area

--- script.js
+++ script.js
@@ -6,7 +6,15 @@
 
 "use strict";
 
+const items = [];
+
 const main = document.getElementById("main");
+
+const itemsDiv = document.getElementById("items");
+const itemsDivRect = itemsDiv.getBoundingClientRect();
+
+const BASE_WINDOW_WIDTH = 1280;
+const BASE_WINDOW_HEIGHT = 720;
 
 class Item {
   constructor(id, url, alt, x, y, w, h, z = 0) {
@@ -14,10 +22,14 @@ class Item {
 
     this.oldWindowInnerWidth = window.innerWidth;
 
+    this.posX = 0;
+    this.posY = 0;
+
     this.offsetX = 0;
     this.offsetY = 0;
 
     /* Flags */
+    this.flagGrabbed = false;
     this.flagGrabbable = true;
     this.flagFirstGrabbed = false;
     this.flagFirstDropped = false;
@@ -28,6 +40,19 @@ class Item {
 
     this.grabbedCallback = null;
     this.droppedCallback = null;
+
+    this.hoverCallback = () => {};
+
+    this.scrollCallback = () => {
+      if (!this.flagGrabbed) {
+        return;
+      }
+
+      const e = this.element ?? this.getElement();
+
+      this.posY += window.screenY;
+      e.style.top = this.posY + "px";
+    };
 
     this.startDragging = (e) => {
       e.preventDefault();
@@ -41,8 +66,9 @@ class Item {
       if (this.flagGrabbable) {
         this.element = this.getElement();
 
-        this.offsetX = e.clientX - this.element.getBoundingClientRect().left;
-        this.offsetY = e.clientY - this.element.getBoundingClientRect().top;
+        const rect = this.element.getBoundingClientRect();
+        this.offsetX = e.clientX - rect.left;
+        this.offsetY = e.clientY - window.scrollY - rect.top;
         this.element.classList.add("dragging");
 
         document.addEventListener("mousemove", this.dragElement);
@@ -52,11 +78,13 @@ class Item {
     this.dragElement = (e) => {
       e.preventDefault();
 
-      const x = e.clientX - this.offsetX;
-      const y = e.clientY - this.offsetY;
+      this.flagGrabbed = true;
 
-      this.element.style.left = x + "px";
-      this.element.style.top = y + "px";
+      this.posX = e.clientX - this.offsetX;
+      this.posY = e.clientY - this.offsetY;
+
+      this.element.style.left = this.posX + "px";
+      this.element.style.top = this.posY + "px";
     };
 
     this.stopDragging = () => {
@@ -65,14 +93,18 @@ class Item {
         this.firstDroppedCallback?.();
       }
 
+      this.flagGrabbed = false;
       this.droppedCallback?.();
 
       document.removeEventListener("mousemove", this.dragElement);
+      this.element?.classList.remove("dragging");
+
       this.element = null;
     };
 
-    this.create(id, url, alt, main, w, h, z);
-    this.moveTo(x, y);
+    this.create(id, url, alt, itemsDiv, w, h, z);
+
+    this.moveTo(x + itemsDivRect.left, y + itemsDivRect.top);
   }
 
   create(id, url, alt, under, w, h, z) {
@@ -93,9 +125,9 @@ class Item {
     div.appendChild(img);
     div.addEventListener("mousedown", this.startDragging);
     div.addEventListener("mouseup", this.stopDragging);
+    div.addEventListener("mouseenter", this.hoverCallback);
 
     window.addEventListener("resize", () => {
-      console.log("CALLED");
       const e = this.element ?? this.getElement();
       const rect = e.getBoundingClientRect();
 
@@ -107,6 +139,8 @@ class Item {
       this.moveTo(newX, rect.top);
     });
 
+    window.addEventListener("scroll", this.scrollCallback);
+
     under.appendChild(div);
   }
 
@@ -114,7 +148,10 @@ class Item {
     const e = this.element ?? this.getElement();
 
     this.offsetX = x;
-    this.offsetY = y;
+    this.offsetY = y + window.screenY;
+
+    this.posX = this.offsetX = x;
+    this.posY = this.offsetY = y + window.screenY;
 
     e.style.left = this.offsetX + "px";
     e.style.top = this.offsetY + "px";
@@ -122,6 +159,9 @@ class Item {
 
   setGrabbable(grabbable) {
     this.flagGrabbable = grabbable;
+
+    const e = this.element ?? this.getElement();
+    e.style.cursor = grabbable ? "grab" : "pointer";
   }
 
   setFirstGrabbedCallback(callback) {
@@ -140,12 +180,38 @@ class Item {
     this.droppedCallback = callback;
   }
 
+  setHoverCallback(callback) {
+    this.hoverCallback = callback;
+  }
+
   getElement() {
     return document.getElementById(this.id);
   }
 }
 
-const items = [];
+function addZone(description, url, alt) {
+  const div = document.createElement("div");
+  div.classList.add("zone");
+
+  const p = document.createElement("p");
+  p.classList.add("inner-description");
+  p.innerText = description;
+
+  const img = document.createElement("img");
+  img.classList.add("inner-image");
+  img.setAttribute("src", url);
+  img.setAttribute("alt", alt);
+
+  div.appendChild(p);
+  div.appendChild(img);
+
+  main.appendChild(div);
+}
+
+function playAudio(src) {
+  const audio = new Audio(src);
+  audio.play();
+}
 
 function spawnItem(item) {
   items.push(item);
@@ -156,16 +222,17 @@ function spawnNormalPaper() {
     "normal_paper",
     "assets/img_normal_paper.png",
     'A taped sheet of paper. It reads: "normal boring place, no secrets!"',
-    992,
-    528,
+    544,
+    400,
     320,
     120,
+    2,
   );
 
   normal_paper.setFirstGrabbedCallback(() => {
     window.setTimeout(() => {
-      const stinger = new Audio("assets/snd_dramatic_stinger.mp3");
-      stinger.play();
+      document.title = "The FNESB";
+      playAudio("assets/snd_dramatic_stinger.mp3");
     }, 500);
   });
 
@@ -177,8 +244,8 @@ function spawnFrozenTombSign() {
     "frozen_tomb_sign",
     "assets/img_frozen_tomb_sign.png",
     'A sign. It reads: "Frozen Tomb," with a red arrow pointing left',
-    600,
-    480,
+    176,
+    320,
     180,
     180,
   );
@@ -191,9 +258,71 @@ function spawnFrozenTombSign() {
   spawnItem(frozen_tomb_sign);
 }
 
+function spawnSmallRock() {
+  const small_rock = new Item(
+    "small_rock",
+    "assets/img_small_rock.png",
+    "A small inconspicuous rock",
+    384,
+    448,
+    64,
+    64,
+    1,
+  );
+
+  spawnItem(small_rock);
+}
+
+function spawnEntranceButton() {
+  const entrance_button = new Item(
+    "entrance_button",
+    "assets/img_entrance_button.png",
+    "A big red button on the ground",
+    400,
+    456,
+    32,
+    32,
+    0,
+  );
+
+  entrance_button.setGrabbable(false);
+  entrance_button.setFirstGrabbedCallback(() => {
+    playAudio("assets/snd_button_accept.mp3");
+    window.setTimeout(addBunkerReceptionZone, 1000);
+  });
+
+  spawnItem(entrance_button);
+}
+
+function spawnBunkerHatch() {
+  const hatch = new Item(
+    "bunker_hatch",
+    "assets/img_bunker_hatch.png",
+    "An open hatch, leading below",
+    176,
+    496,
+  );
+
+  hatch.setGrabbable(false);
+
+  spawnItem(hatch);
+}
+
+function addBunkerReceptionZone() {
+  spawnBunkerHatch();
+  playAudio("assets/snd_clue_stinger.mp3");
+  addZone(
+    "Woah...",
+    "assets/img_bg_reception_top.png",
+    "A quaint little reception area. There's a desk and everything.",
+  );
+}
+
 function init() {
   spawnNormalPaper();
   spawnFrozenTombSign();
+  spawnSmallRock();
+  spawnEntranceButton();
 }
 
 init();
